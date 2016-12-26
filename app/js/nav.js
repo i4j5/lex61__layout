c20f22a31bbc20257bf875f83da4e02f004865df
Aleksandr Sazhin <i4j5@ya.ru>
2016-12-26 21:38:45 +0300

    nav

--- app/js/nav.js
+++ app/js/nav.js
@@ -1,8 +1,8 @@
 (()=>{
 
-    let toggle = document.getElementById('nav__toggle');
-    let wrapper = document.getElementById('nav__wrapper');
-    let close = document.getElementById('nav__close');
+    const toggle = document.getElementById('nav__toggle');
+    const wrapper = document.getElementById('nav__wrapper');
+    const close = document.getElementById('nav__close');
 
     toggle.addEventListener('click', () => {
         wrapper.classList.add('nav__wrapper_active');
